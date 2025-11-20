73859dd32b5090439468aa39adc540bb9486d49e
Miguel-Ankido <miguelcm553@gmail.com>
2025-11-20 18:32:19 -0300

    fix vercel3

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: 'produto/:id', 
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '**', 
     renderMode: RenderMode.Prerender
   }
 ];
